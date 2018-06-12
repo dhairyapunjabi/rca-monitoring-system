f4111728022f6670c97201a72367a13cad4606c2
Dhairya Punjabi <dhairyapunjabi@Dhairyas-MacBook-Pro.local>
2018-06-12 16:07:52 +0530

    Refactor the test for model

--- spec/models/rca_spec.rb
+++ spec/models/rca_spec.rb
@@ -1,9 +1,10 @@
 require 'rails_helper'
 
 RSpec.describe Rca, type: :model do
-  context "When testing the database type" do
-    it "should be Mongoid Document" do
+  describe "new object" do
+    it "is a Mongoid document" do
       is_expected.to be_mongoid_document
     end
   end
+
 end
