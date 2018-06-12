8e7f5b17f5d1a40db61d7d6d169826ceb272f908
Dhairya Punjabi <dhairyapunjabi@Dhairyas-MacBook-Pro.local>
2018-06-12 15:44:31 +0530

    Configure required files to make simplecov and factory bot work
    factory_bot.rb and rails_helper.rb are configured

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -1,13 +1,13 @@
 require 'spec_helper'
 require 'mongoid-rspec'
 require 'support/factory_bot'
+require 'simplecov'
+SimpleCov.start 'rails'
 ENV['RAILS_ENV'] ||= 'test'
 require File.expand_path('../../config/environment', __FILE__)
 
 abort("The Rails environment is running in production mode!") if Rails.env.production?
 require 'rspec/rails'
-require 'simplecov'
-Simplecov.start 'rails'
 
 RSpec.configure do |config|
   config.infer_spec_type_from_file_location!
