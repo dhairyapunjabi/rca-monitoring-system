c9f7cfac94e122219af6ba5974a2bc5a40fa1797
Punit Lakshwani <punit97@Punits-MacBook-Pro.local>
2018-06-18 16:05:11 +0530

    Add email field in User model

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -6,4 +6,10 @@ RSpec.describe User, type: :model do
       is_expected.to be_mongoid_document
     end
   end
+
+  describe 'data type' do
+    it 'of email-address is string' do
+      is_expected.to have_field(:email).of_type(String)
+    end
+  end
 end
