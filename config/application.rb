32f7047f898ff96507c3d126a556084c83d8e860
Dhairya Punjabi <dhairyapunjabi@Dhairyas-MacBook-Pro.local>
2018-07-01 22:00:33 +0530

    Remove field-with-errors wrapping of html elements

--- config/application.rb
+++ config/application.rb
@@ -15,5 +15,6 @@ Bundler.require(*Rails.groups)
 module RcaMonitoringSystem
   class Application < Rails::Application
     config.load_defaults 5.2
+    config.action_view.field_error_proc = Proc.new { |html_tag, instance | html_tag }
   end
 end
