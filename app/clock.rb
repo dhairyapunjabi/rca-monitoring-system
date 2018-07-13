9a26ae305987b9015cd80eb48e070847ebf500a8
Punit Lakshwani <punit97@Punits-MacBook-Pro.local>
2018-07-13 19:38:46 +0530

    Update scheduler for slack notifier

--- app/clock.rb
+++ app/clock.rb
@@ -4,11 +4,19 @@ require './config/boot'
 require './config/environment'
 require 'slack-notifier'
 
-every(1.day,'send_email', at: '00:00') do
+every(1.day, 'send_email', at: '00:00') do
   actionitems = Actionitem.all.to_a
   actionitems.each do |actionitem|
     if (actionitem.status == 'Pending' && Date.today == actionitem.complete_by)
       UserMailer.with(user: actionitem.rca.user, actionitem: actionitem).reminder_email.deliver_now
+    end
+  end
+end
+
+every(1.day, 'send_notification',at: '00:00') do
+  actionitems = Actionitem.all.to_a
+  actionitems.each do |actionitem|
+    if (actionitem.status == 'Pending' && Date.today == actionitem.complete_by)
       notifier = Slack::Notifier.new actionitem.rca.team.slack_webhook_url
       notifier.ping "RCA: #{actionitem.rca.title} \n Your action item '#{actionitem.name}' submitted by #{actionitem.rca.user.email} is still pending.\n Attend to it asap."
     end
