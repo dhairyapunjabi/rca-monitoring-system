884df02aa3e3f6621a53a5e309251e1185eb72fd
Punit Lakshwani <punit97@Punits-MacBook-Pro.local>
2018-07-13 18:51:33 +0530

    Add slack notifier for sending notification to team channel
    Send action item detail to team channel for every pending action item not completed on its scheduled date

--- Gemfile
+++ Gemfile
@@ -22,6 +22,7 @@ gem 'jquery-easing-rails', '~> 0.0.2'
 gem 'jquery-datatables', '~> 1.10', '>= 1.10.19'
 gem 'jquery-ui-rails', github: 'jquery-ui-rails/jquery-ui-rails'
 gem 'clockwork', '~> 2.0', '>= 2.0.3'
+gem "slack-notifier"
 
 group :development, :test do
   gem 'byebug', platforms: [:mri, :mingw, :x64_mingw]
--- app/clock.rb
+++ app/clock.rb
@@ -2,12 +2,15 @@ require 'clockwork'
 include Clockwork
 require './config/boot'
 require './config/environment'
+require 'slack-notifier'
 
 every(1.day,'send_email', at: '00:00') do
-    actionitems = Actionitem.all.to_a
-    actionitems.each do |actionitem|
-      if (actionitem.status == 'Pending' && Date.today == actionitem.complete_by)
-        UserMailer.with(user: actionitem.rca.user, actionitem: actionitem).reminder_email.deliver_now
-      end
+  actionitems = Actionitem.all.to_a
+  actionitems.each do |actionitem|
+    if (actionitem.status == 'Pending' && Date.today == actionitem.complete_by)
+      UserMailer.with(user: actionitem.rca.user, actionitem: actionitem).reminder_email.deliver_now
+      notifier = Slack::Notifier.new actionitem.rca.team.slack_webhook_url
+      notifier.ping "RCA: #{actionitem.rca.title} \n Your action item '#{actionitem.name}' submitted by #{actionitem.rca.user.email} is still pending.\n Attend to it asap."
     end
+  end
 end
